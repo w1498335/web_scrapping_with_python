797b9790a63a3b2b11c42dd3d3fbb14219a0b9c5
Ali Zamani <alizamani148@yahoo.com>
2022-11-30 22:39:50 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
 #Here i am going to show you the basics of how to scrape a website using BeautifulSoup
 
-#  Requirments first open your terminal on pycharm and install bs4 using: -pip install bs4
-import requests
-# secondly install requests using the same terminal:         -pip install requests
+#  Requirments: first open your terminal on pycharm and install bs4 using: -pip install bs4
 from bs4 import BeautifulSoup
+# secondly install requests using the same terminal:  -pip install requests
+import requests
+
 
 #The website below is the website we are going to scrape
 url = "https://www.wizardingworld.com/news/fantastic-beasts-secrets-of-dumbledore-to-be-released-globally-april-2022"
